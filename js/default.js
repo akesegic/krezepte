1cfd9f509db7322d8f7cf07dea077f52d4aad367
Gabrijel Skrenković <gabrijel.skrenkovic@gmail.com>
2014-05-14 00:28:05 +0200

    Responsive and popup functionality

--- js/default.js
+++ js/default.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
 	
-	$(".info-box").click(function(){
-		$(".popup").fadeIn(150);
+	//Show popup
+	$(".info-box").click(function(event){
+		event.preventDefault();
+		
+		$(".js-popup1").fadeIn(150);
 	});
 	
+	//Hide popup
 	$(".popup").click(function(event){
 		var target = $(event.target);
-		if (target.attr("class") == "popup") {
+		
+		if (target.hasClass("popup")) {
 			$(".popup").fadeOut(150);
 		}
+	});
+	
+	//Switch to second popup
+	$(".js-trigger-pin").click(function(event){
+		event.preventDefault();
+		
+		$(".js-popup1").fadeOut(150);
+		$(".js-popup2").fadeIn(150);
 	});
 	
 });
